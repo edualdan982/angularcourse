52b8997a2b2e287320c716b06c5b080f0bb787ba
edsarmiento1 <edsarmiento1@umsa.bo>
2020-10-22 10:57:02 -0400

    promesas1

--- typescript/app.js
+++ typescript/app.js
@@ -1,42 +1,14 @@
 "use strict";
 //funcion anomina no invicada
-(function () {
-    var avenger = {
-        nombre: 'Steve',
-        clave: 'Capitan América',
-        poder: 'Droga'
-    };
-    //No importa el orden
-    var nombre = avenger.nombre, clave = avenger.clave, poder = avenger.poder;
-    console.log('Mediante acceso al objeto.');
-    console.log(avenger.nombre);
-    console.log(avenger.clave);
-    console.log(avenger.poder);
-    console.log('Mediante destruracion del objeto.');
-    console.log(nombre);
-    console.log(clave);
-    console.log(poder);
-    var extraer = function (_a) {
-        var nombre = _a.nombre, poder = _a.poder;
-        //const { nombre, poder } = avenger;
-        console.log(nombre);
-        console.log(poder);
-    };
-    //extraer(avenger);
-    var avengers = ['Thor', 'Iron Man', 'Spiderman'];
-    console.log(avengers[0]);
-    console.log(avengers[1]);
-    console.log(avengers[2]);
-    console.log('Destruraciion de  Arreglos');
-    var loki = avengers[0], hombre = avengers[1], araña = avengers[2];
-    // console.log( loki );
-    // console.log( hombre );
-    console.log(araña);
-    var extraerArr = function (_a) {
-        var loki = _a[0], hombre = _a[1];
-        //const { nombre, poder } = avenger;
-        console.log(loki);
-        console.log(hombre);
-    };
-    extraerArr(avengers);
+(() => {
+    console.log('Inicio');
+    const prom1 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject('Se termino el timeout');
+        }, 1000);
+    });
+    prom1
+        .then(mensaje => console.log(mensaje))
+        .catch(err => console.warn(err));
+    console.log('Fin');
 })();
