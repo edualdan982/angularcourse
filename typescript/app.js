1aeee8a627f94d15197fd5bc109c6c8deae95034
edsarmiento1 <edsarmiento1@umsa.bo>
2020-10-22 22:34:06 -0400

    clases

--- typescript/app.js
+++ typescript/app.js
@@ -1,16 +1,36 @@
 "use strict";
 //funcion anomina no invicada
 (() => {
-    const enviarMission = (xmen) => {
-        console.log(`Enviando a ${xmen.nombre} a la misión.`);
-    };
-    const regresarAlCuartel = (xmen) => {
-        console.log(`Enviando a ${xmen.nombre} al cuartel.`);
-    };
-    const wolverine = {
-        nombre: 'Logan',
-        edad: 60,
-    };
-    enviarMission(wolverine);
-    regresarAlCuartel(wolverine);
+    // class Avenger {
+    //   nombre: string;
+    //   equipo: string;
+    //   nombreReal: string;
+    //   puedePelear: boolean;
+    //   peleasGanadas: number;
+    //   constructor(nombre: string, equipo: string) {
+    //     this.nombre = nombre;
+    //     this.equipo = equipo;
+    //     // this.nombreReal = nombreReal;
+    //     // this.puedePelear = puedePelear;
+    //     // this.peleasGanadas= peleasGanadas:
+    //   }
+    // }
+    // const antMan = new Avenger('AntMan', 'Capitan');
+    // console.log(antMan);
+    class Avenger {
+        // nombre: string;
+        // equipo: string;
+        // nombreReal: string;
+        // puedePelear: boolean;
+        // peleasGanadas: number;
+        constructor(nombre, equipo, nombreReal, puedePelear = true, peleasGanadas = 0) {
+            this.nombre = nombre;
+            this.equipo = equipo;
+            this.nombreReal = nombreReal;
+            this.puedePelear = puedePelear;
+            this.peleasGanadas = peleasGanadas;
+        }
+    }
+    const antMan = new Avenger('AntMan', 'Capi');
+    console.log(antMan);
 })();
